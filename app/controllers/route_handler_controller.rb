f7fa0e62500eb795aa8e7e8ec55cdddc66780717
Anton Astashov <anton@localhost.(none)>
2009-02-18 09:35:42 +0700

    Added open of binded page

--- app/controllers/route_handler_controller.rb
+++ app/controllers/route_handler_controller.rb
@@ -1,14 +1,23 @@
-class RouteHandlerController < ApplicationController
+class RouteHandlerController < SiteController
   
   no_login_required
   
+  
   def index
-#    route_handler = RouteHandler.new(params[:path])
-#    if params[:path].join("/").match(/(daily|weekly|monthly)\/(\w+)\/(\w+)\/(\w+)/)
-#      render :text => "Success!"
-#    else
-#      redirect_to '/404.html'
-#    end
+    @route_handler = RouteHandler.match(params[:path])
+    if @route_handler
+      show_page
+    else
+      redirect_to '/404.html'
+    end
   end
+  
+  
+  private
+  
+    def find_page(*args)
+      found = @route_handler.page
+      found if found and (found.published? or dev?)
+    end
   
 end
---
+++ spec/controllers/route_handler_controller_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe RouteHandlerController do
+  integrate_views
+  before do
+    @page = Page.create!(
+      :title => 'New Page',
+      :slug => 'page',
+      :breadcrumb => 'New Page',
+      :status_id => '1',
+      :parts => [{:name => "body", :content => "Hi there!"}],
+      :status_id => 100 # Published
+    )
+    @route_handler = RouteHandler.create!(
+      :url => '^(\w+)\/(\w+)$', 
+      :transformation_rules => "",
+      :description => "",
+      :fields => "name, date",
+      :page => @page
+    )
+  end
+  
+  it "should open specified page" do
+    get :index, :path => [ 'dailyoverview', 'today' ]
+    response.should be_success
+    assigns(:route_handler).should == @route_handler
+    response.body.should include('Hi there!')
+  end
+  
+  it "should show 404 error if there are no matched route handlers" do
+    get :index, :path => [ 'daily', 'overview', 'today' ]
+    response.should redirect_to('/404.html')
+    assigns(:route_handler).should be_nil
+  end
+
+end
