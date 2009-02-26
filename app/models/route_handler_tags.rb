424acb3830c89c63806aba0d0cade90b350e77ba
Anton Astashov <anton@localhost.(none)>
2009-02-26 10:04:46 +0700

    Added description for <r:route_param> tag

--- app/models/route_handler_tags.rb
+++ app/models/route_handler_tags.rb
@@ -1,6 +1,14 @@
 module RouteHandlerTags
   include Radiant::Taggable
 
+  desc %{
+   Shows some value from route handler parameters. It has only one attribute - @name@, 
+   that contains name of value.
+   
+   *Usage:*
+   
+   <pre><code><r:route_param name="value_name" /></code></pre>
+  }
   tag 'route_param' do |tag|
     params = tag.locals.page.route_handler_params
     params && params[tag.attr['name'].to_sym]
