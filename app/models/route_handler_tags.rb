ba0cdfe383f9df5db0a82c05d0ffd4dde3069930
Anton Astashov <anton@astashov.net>
2009-08-16 14:41:11 +0800

    Added if param == value and unless param == value conditions (see details in README, Details/3)

--- app/models/route_handler_tags.rb
+++ app/models/route_handler_tags.rb
@@ -3,15 +3,47 @@ module RouteHandlerTags
 
   desc %{
    Shows some value from route handler parameters. It has only one attribute - @name@, 
-   that contains name of value.
+   that contains name of parameter.
    
    *Usage:*
    
-   <pre><code><r:route_param name="value_name" /></code></pre>
+   <pre><code><r:route_param name="parameter_name" /></code></pre>
   }
   tag 'route_param' do |tag|
     params = tag.locals.page.route_handler_params
     params && params[tag.attr['name'].to_sym]
+  end
+
+
+  desc %{
+    Execute block if route handler parameter equal to specified value. It has attributes
+    @name@ and @value@, where @name@ contains name of parameter and @value@ contains
+    value that should be equal to.
+
+    *Usage:*
+
+    <pre><code><r:if_route_param_equals name="param_name" value="value"> some code </r:if_route_param_equals></code></pre>
+  }
+  tag 'if_route_param_equals' do |tag|
+    params = tag.locals.page.route_handler_params
+    show_block = params[tag.attr['name'].to_sym] == tag.attr['value']
+    tag.expand if show_block
+  end
+
+
+  desc %{
+    Execute block if route handler parameter *not* equal to specified value. It has attributes
+    @name@ and @value@, where @name@ contains name of parameter and @value@ contains
+    value that should be equal to.
+
+    *Usage:*
+
+    <pre><code><r:unless_route_param_equals name="param_name" value="value"> some code </r:unless_route_param_equals></code></pre>
+  }
+  tag 'unless_route_param_equals' do |tag|
+    params = tag.locals.page.route_handler_params
+    show_block = params[tag.attr['name'].to_sym] != tag.attr['value']
+    tag.expand if show_block 
   end
   
 end
--- spec/models/route_handler_tags_spec.rb
+++ spec/models/route_handler_tags_spec.rb
@@ -21,5 +21,27 @@ describe RouteHandlerTags do
     @page.route_handler_params = {:name => 'something', :something => "else"}
     @page.should render("<r:route_param name='_:name:_' />").as('else')
   end
+
+  
+  it "should execute inner block if condition is true" do
+    @page.route_handler_params = {:name => 'something'}
+    @page.should render(
+      "<r:if_route_param_equals name='name' value='bla'>Text</r:if_route_param_equals>"
+    ).as('')
+    @page.should render(
+      "<r:if_route_param_equals name='name' value='something'>Text</r:if_route_param_equals>"
+    ).as('Text')
+  end
+
+  it "should execute inner block unless condition is true" do
+    @page.route_handler_params = {:name => 'something'}
+    @page.should render(
+      "<r:unless_route_param_equals name='name' value='bla'>Text</r:unless_route_param_equals>"
+    ).as('Text')
+    @page.should render(
+      "<r:unless_route_param_equals name='name' value='something'>Text</r:unless_route_param_equals>"
+    ).as('')
+  end
+
   
 end
